99b4f543ccc0ae781b630b3fdd4ecdb9b49eee93
lukasbloom93 <lukas.bloom@hotmail.se>
2020-10-17 17:50:43 +0200

    Fifth Commit. Added string toString in Player class and added new game() to main.

--- src/com/company/Player.java
+++ src/com/company/Player.java
@@ -1,10 +1,25 @@
 package com.company;
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class Player {
     String name;
-    int gold = 500;
+    int gold = 100;
     ArrayList<Animal> playerAnimals;
     ArrayList<Food> playerFood;
 
+    public Player(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "Player{" +
+                "name='" + name + '\'' +
+                ", gold=" + gold +
+                ", playerAnimals=" + playerAnimals +
+                ", playerFood=" + playerFood +
+                '}';
+    }
 }
+
