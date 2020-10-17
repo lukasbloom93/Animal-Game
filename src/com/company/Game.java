0066f9017162e726dae094c10751174154e2e368
lukasbloom93 <lukas.bloom@hotmail.se>
2020-10-17 17:54:20 +0200

    Sixth Commit. Iterated on methods in main and added several methods. Also added do-while code to help manage errors.

--- src/com/company/Game.java
+++ src/com/company/Game.java
@@ -3,86 +3,139 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Game {
-    public Game(){
-
     boolean gameOn = true;
+    boolean isValid = false;
     final Scanner scan = new Scanner(System.in);
     int playerCount;
     int roundCounter = 0;
-    int round;
-    ArrayList<String> players;
+    int round = 0;
+    ArrayList<Player> allPlayers;
+    int totalRounds;
+    int activePlayer = 0;
 
-    String temp = players.get(0);
+    public Game() {
+        setupGame();
+        runGame();
+    }
+    public void setupGame() {
+            do {
+                System.out.println("Welcome to Animal Farm!"
+                + "\n How many players will be playing? (Type a number, 1-4 and press enter to select)");
 
-    public void setupGame(){
-        System.out.println("Welcome to Animal Farm!"
-                +"\n How many players will be playing? (Type a number, 1-4 and press enter to select)");
+                playerCount = scan.nextInt();
+                if (playerCount > 4 || playerCount < 1){
+                    System.out.println("Try again!");
+                }
+                else{
+                    isValid = true;
+                }
+            } while (!isValid);
 
-        scan.nextInt() = playerCount;
+            for (var i = 1; i <= playerCount; i++) {
+                System.out.println("Enter player " + i + "'s name: ");
+                String playerName = scan.next();
+                Player player = new Player(playerName);
+                allPlayers.add(player);
+            }
 
-        for(var i = 1; i <= playerCount; i++) {
-            System.out.println("Enter player " + i + "'s name: ");
-            players.add((scan.next()));
-        }
+            isValid = false;
+            do {
+                System.out.println("Please choose the number of rounds you would like to play."
+                        + "\n Enter a number between 5 and 30");
+                try {
+                    round = scan.nextInt();
+                }catch (Exception e){
+                    round = 0;
+                }
+                if (round > 30 || round < 5){
+                    System.out.println("Try again!");
+                }
+                else{
+                    isValid = true;
+                }
 
-        System.out.println("Please choose the number of rounds you would like to play."
-                + "\n Enter a number between 5 and 30");
+            } while (!isValid);
 
-        scan.nextInt() = round;
-        int totalRounds = playerCount*round;
+            totalRounds = playerCount * round;
     }
 
-       while (gameOn){
+    public void checkInventory(){
+        System.out.println(this.allPlayers.get(activePlayer).toString());
+    }
 
-        public void turnAction(){
-        System.out.println("It is now " + "'s turn!"
-                + "\n What would you like to do? You may only perform one action per turn."
-                + "\n Type an integer and press enter to continue."
-                + "\n"
-                + "\n 1. Buy Animals"
-                + "\n 2. Buy Food"
-                + "\n 3. Feed Animals"
-                + "\n 4. Mate Animals"
-                + "\n 5. Sell Animals"
-                + "\n 6. Check Inventory (Does not count as an action)");
-
-        scan.nextInt() = int action;
-
-        int choice = switch (action) {
-            case 1 -> buyAnimal();
-            case 2 -> buyFood();
-            case 3 -> feedAnimal();
-            case 4 -> mateAnimal();
-            case 5 -> sellAnimal();
-            case 6 -> checkInventory();
-            default -> retry turnAction
+    public void runGame(){
+        while(gameOn) {
+            turnAction();
+            rounds();
         }
+    }
 
-        public void rounds () {
+        public void turnAction() {
+            System.out.println("It is now " + allPlayers.get(activePlayer).name + "'s turn!");
+
+
+            Player player = allPlayers.get(activePlayer);
+            player.playerAnimals.forEach((animal) -> {
+                if (animal.hitPoints <= 0) {
+                    player.playerAnimals.remove(animal);
+                    System.out.println(animal.name + "starved to death!");
+                }
+            });
+
+            int action;
+            isValid = false;
+
+            checkInventory();
+
+            do {
+                System.out.println("\n What would you like to do? You may only perform one action per turn."
+                        + "\n If your gold at any point reaches 0 you have lost the game!"
+                        + "\n Type an integer and press enter to continue."
+                        + "\n"
+                        + "\n 1. Buy Animals"
+                        + "\n 2. Buy Food"
+                        + "\n 3. Feed Animals"
+                        + "\n 4. Mate Animals"
+                        + "\n 5. Sell Animals"
+                        + "\n 6. Check Inventory (Does not count as an action)");
+
+                action = scan.nextInt();
+                if (action > 5 || action < 1) {
+                    System.out.println("Try again!");
+                } else {
+                    isValid = true;
+                }
+            } while (!isValid);
+
+
+            switch (action) {
+                case 1 -> Store.buyAnimalMenu(player);
+                case 2 -> Store.buyFoodMenu(player);
+                case 3 -> Animal.feedAnimalMenu(player);
+                case 4 -> Animal.mateAnimalMenu(player);
+                case 5 -> Store.sellAnimalMenu(player);
+            }
+
+            player.playerAnimals.forEach(Animal::decreaseHP);
+
+            if (player.gold <= 0) {
+                System.out.println(player.name + "is out of the game!");
+            }
+            roundCounter++;
+            activePlayer = (activePlayer + 1)% playerCount;
+        }
+        public void rounds(){
             if (roundCounter >= totalRounds) {
                 System.out.println("The game is over!");
 
-                /* I need to find the player with the most gold, use sort */
+                allPlayers.sort((Player a, Player b) -> { return a.gold > b.gold ? -1 : 1; });
+                System.out.println(allPlayers.get(0) + "has won the game!");
+
                 gameOn = false;
             }
         }
     }
-       }
-}
-}
 
-    public void players(){}
 
-    public void checkInventory(){
-        System.out.println(this.players.gold+" "+this.players.playersAnimals+" "+this.players.playerFood);
-    }
 
-   public void buyAnimal(){}
 
-    public void sellAnimal(){}
-
-    public void feedAnimal(){}
-
-    public void buyFood(){}
-
-    public void mateAnimal(){}
